81a19002fb5d63d1c3c0213e31f86d51dc0f7075
jonypx19 <jonathan.pelastine@mail.utoronto.ca>
2016-07-10 23:59:21 -0400

    Added a new folder called p1 that contains the required files for phase 1; made additional changes for index.html

---
+++ p1/assets/scripts/vehicles.js
@@ -0,0 +1,92 @@
+$(document).ready(main);
+
+function getVehicles() {
+	$.ajax({
+		method: "Get",
+		url: "/vehicles/listVehicles",
+		data: {user: "bob"},
+		dataType: "json",
+		success: function(json) {
+			var user_cars = json;
+			console.log(user_cars);
+			for (var i = 0; i < user_cars.length; i++) {
+				var car_article = $('<article>').appendTo('section#list_vehicles');
+
+				$('<span>', {
+					class: 'vehicle_info',
+					text: user_cars[i].manufacturer
+				}).appendTo(car_article);
+
+				$('<span>', {
+					class: 'vehicle_info',
+					text: user_cars[i].model
+				}).appendTo(car_article);
+
+				$('<span>', {
+					class: 'vehicle_info',
+					text: user_cars[i].year
+				}).appendTo(car_article);
+
+				$('<span>', {
+					class: 'vehicle_info',
+					text: user_cars[i].license
+				}).appendTo(car_article);
+
+				$('<img>', {
+					class: 'vehicle_info',
+					src: user_cars[i].image
+				}).appendTo(car_article);
+			}
+
+		}
+	})
+}
+
+/*Updates page with a contract form */
+function createContractForm(car_values) {
+	if (typeof car_values == 'undefined') {
+		car_values = {};
+	}
+
+	if ($('section#contract_form article div#car_select').length > 0) {
+		$('section#contract_form article div#car_select').remove();
+	}
+
+	var selected_info = $('<div>', {id: 'car_select'}).insertAfter('section#contract_form article h2.selected');
+
+	console.log(car_values);
+
+	var contract_article = $('section#contract_form article');
+
+	
+
+
+	contract_article.data('contract_car', car_values);
+
+	$('<span>', {
+		class: 'contract_info',
+		text: car_values.manufacturer
+	}).appendTo(selected_info);
+
+	$('<span>', {
+		class: 'contract_info',
+		text: car_values.model
+	}).appendTo(selected_info);
+
+	$('<span>', {
+		class: 'contract_info',
+		text: car_values.year
+	}).appendTo(selected_info);
+
+	$('<span>', {
+		class: 'contract_info',
+		text: car_values.license
+	}).appendTo(selected_info);
+
+}
+
+/*Attaches controller functionality to necessary buttons and Forms*/
+function main() {
+	getVehicles();
+
+}
