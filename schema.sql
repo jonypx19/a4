a847b1fcc6ac845a9b37462807fb8f4dd2fe0238
Fullchee Zhang <fullchee@outlook.com>
2016-07-22 11:28:18 -0400

    Added users and changed year to model_year

--- schema.sql
+++ schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE vehicle (
 	id INTEGER PRIMARY KEY,
 	make TEXT NOT NULL,
 	model TEXT NOT NULL,
-	year INTEGER NOT NULL,
+	model_year INTEGER NOT NULL,
 	license_plate TEXT NOT NULL,
 	ownerid INTEGER NOT NULL,
 	image TEXT,
@@ -52,9 +52,26 @@ CREATE TABLE review (
 	id INTEGER PRIMARY KEY,
 	subjectid INTEGER NOT NULL,
 	authorid INTEGER NOT NULL,
+	contractid INTEGER NOT NULL,
 	content TEXT,
 	rating INTEGER,
 	
 	FOREIGN KEY(subjectid) REFERENCES user(id),
 	FOREIGN KEY(authorid) REFERENCES user(id),
 );
+
+INSERT INTO users (id, name, email, password)
+	VALUES (1, 'super admin', 'info@cardetailexchange.ca', '$2a$10$ym4IYLE0OKUMJQCPhC.njenpg4XfmKtnXlrz23nQ/1IJPSrSQckoa');
+
+-- password: 'thank you'
+INSERT INTO users (id, name, email, password)
+	VALUES (12, 'Terry Yan', 'ty@ty.ty', '$2a$10$ym4IYLE0OKUMJQCPhC.njenpg4XfmKtnXlrz23nQ/1IJPSrSQckoa');
+	
+	-- password: mysterion
+INSERT INTO users (id, name, email, password)
+	VALUES (11, 'Mysterion', 'whois@myserion.com', '$2a$10$kA6DAeHoxNYMyYR5Svs3Ae8RPyKmBzbiEC8J0zGrfbB9qGb9lYOHq');
+
+-- owned by user 11
+INSERT INTO vehicle (id, make, model, model_year, license_plate, ownerid, image)
+	VALUES (1, 'Mystery Machine', 'N/A', '1969', 'AAAA 111', '11', 'images/placeholder_car.jpg');
+	
