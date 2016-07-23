515ff5507f43183f98d636aa3b78c5e6322f75aa
Ross Bevacqua <r.bevacqua94@gmail.com>
2016-07-22 20:21:17 -0400

    added contract creation with geo location feature, also implemented get Contract list

--- model.js
+++ model.js
@@ -25,24 +25,70 @@ Database.prototype.connect = function() {
 
 Database.prototype.insertVehicle = function(username, vehicle, image_data) {
 	
-	this.con.query("INSERT INTO Vehicles (username, manufacturer, model, year, license, image) \
+	this.con.query("INSERT INTO vehicles (ownerid, make, model, year, license_plate, image) \
 		VALUES (?, ?, ?, ?, ?, ?)", 
 		[username, vehicle.manu, vehicle.model, vehicle.year, vehicle.plate, image_data], 
 		function (err, result) {
 			if (err) {
 				console.log("An Error occured during inserting vehicle");
+
 			}
 		});
 };
 
 Database.prototype.getUserVehicles = function(username, callback) {
-	this.query = this.con.query("SELECT * FROM Vehicles WHERE username=?", [username], function(err, result) {
+	this.con.query("SELECT * FROM vehicles WHERE ownerid=?", [username], function(err, result) {
 		if (err) {
 			console.log("Could not find Vehicles Table");
 			callback(err, null);
 		}
 
 		callback(null, result);
+
+	});
+}
+
+Database.prototype.checkContractStatus = function(vehicleid, callback) {
+	this.con.query("SELECT id, vehicleid, status FROM contract WHERE vehicleid=? and (status='available' or status='taken')",
+		[vehicleid],
+		function(err, result) {
+			if (err) {
+				console.log("Couldn't select contracts");
+				callback(err, null);
+			} else if (result.length > 0) {
+				callback(null, true);
+			} else {
+				callback(null, false);
+			}
+		})
+}
+
+Database.prototype.insertContract = function(contract) {
+	this.con.query("INSERT INTO contract (vehicleid, price, full_vacuuming, floor_mats, vinyl_and_plastic, \
+		centre_console, button_cleaning, hand_wash, clean_tires, hand_wax, country, province, city, address, postal_code, latitude, longitude, status) \
+		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+		[contract.vehicleid, contract.price, contract.vacuum, contract.mats, contract.protect, contract.console, contract.button_clean, contract.wash, contract.tires, contract.wax, contract.country, contract.province, contract.city, contract.address, contract.postal_code, contract.latitude, contract.longitude, "available"],
+		function (err, result) {
+			if (err) {
+				console.log("Could not insert new Contract");
+				console.log(err);
+			}
+		});
+}
+
+Database.prototype.getUserContracts = function(username, callback) {
+	this.con.query("SELECT contract.id, washerid, vehicleid, price, full_vacuuming, floor_mats, vinyl_and_plastic, \
+		centre_console, button_cleaning, hand_wash, clean_tires, hand_wax, image, make, model, license_plate, year \
+		FROM (contract JOIN vehicles ON contract.vehicleid=vehicles.id) \
+		WHERE ownerid=? or washerid=?", 
+		[username, username], 
+		function(err, result) {
+			if (err) {
+				console.log(err.Error);
+				callback(err, null);
+			}
+
+			callback(null, result);
 
 	});
 }
--- public/assets/scripts/vehicles.js
+++ public/assets/scripts/vehicles.js
@@ -8,7 +8,7 @@ function getVehicles() {
 		dataType: "json",
 		success: function(json) {
 			var user_cars = json;
-			console.log(user_cars);
+			
 			for (var i = 0; i < user_cars.length; i++) {
 				var car_article = $('<article>').appendTo('section#list_vehicles');
 
@@ -19,7 +19,7 @@ function getVehicles() {
 
 				$('<span>', {
 					class: 'vehicle_info',
-					text: user_cars[i].manufacturer
+					text: user_cars[i].make
 				}).appendTo(car_article);
 
 				$('<span>', {
@@ -34,7 +34,7 @@ function getVehicles() {
 
 				$('<span>', {
 					class: 'vehicle_info',
-					text: user_cars[i].license
+					text: user_cars[i].license_plate
 				}).appendTo(car_article);
 
 				var button = $('<button>', {text: 'Create Contract'}).appendTo(car_article);
@@ -43,7 +43,11 @@ function getVehicles() {
 
 				button.on('click', function() {
 
+					$('html, body').animate({
+						scrollTop: $('#contract_form').offset().top -100
+					}, 1000);
 					var car_details = $(this).data('selected_car');
+					$('input#vehicleid').val(car_details.id);
 					createContractForm(car_details);
 
 				})
@@ -77,7 +81,7 @@ function createContractForm(car_values) {
 
 	$('<span>', {
 		class: 'contract_info',
-		text: car_values.manufacturer
+		text: car_values.make
 	}).appendTo(selected_info);
 
 	$('<span>', {
@@ -92,13 +96,94 @@ function createContractForm(car_values) {
 
 	$('<span>', {
 		class: 'contract_info',
-		text: car_values.license
+		text: car_values.license_plate
 	}).appendTo(selected_info);
 
+}
+
+function setPrices() {
+	var total = 0;
+
+	var prices = {
+		wash: 25,
+		tires: 20,
+		wax: 25,
+		vacuum: 20,
+		mats: 10,
+		protect: 20,
+		console: 5,
+		button_clean: 5
+	};
+
+	if ($('label#mats input').prop("checked")) {
+		total += prices.mats;
+		$('label#mats .price').css({color: 'green'});
+	} else {
+		$('label#mats .price').css({color: 'black'});
+	}
+
+	if ($('label#vacuum input').prop("checked")) {
+		total += prices.vacuum;
+		$('label#vacuum .price').css({color: 'green'});
+	} else {
+		$('label#vacuum .price').css({color: 'black'});
+	}
+
+	if ($('label#protect input').prop("checked")) {
+		total += prices.protect;
+		$('label#protect .price').css({color: 'green'});
+	} else {
+		$('label#protect .price').css({color: 'black'});
+	}
+
+	if ($('label#console input').prop("checked")) {
+		total += prices.console;
+		$('label#console .price').css({color: 'green'});
+	} else {
+		$('label#console .price').css({color: 'black'});
+	}
+
+	if ($('label#button_clean input').prop("checked")) {
+		total += prices.button_clean;
+		$('label#button_clean .price').css({color: 'green'});
+	} else {
+		$('label#button_clean .price').css({color: 'black'});
+	}
+
+	if ($('label#wash input').prop("checked")) {
+		total += prices.wash;
+		$('label#wash .price').css({color: 'green'});
+	} else {
+		$('label#wash .price').css({color: 'black'});
+	}
+
+	if ($('label#tires input').prop("checked")) {
+		total += prices.tires;
+		$('label#tires .price').css({color: 'green'});
+	} else {
+		$('label#tires .price').css({color: 'black'});
+	}
+
+	if ($('label#wax input').prop("checked")) {
+		total += prices.wax;
+		$('label#wax .price').css({color: 'green'});
+	} else {
+		$('label#wax .price').css({color: 'black'});
+	}
+
+	
+
+	return total;
 }
 
 /*Attaches controller functionality to necessary buttons and Forms*/
 function main() {
 	getVehicles();
+	$('fieldset.detail label').on('click', function() {
+		var total = setPrices();
+		$('span#total').text('$' + total + '.00');
+		$('input#price').val(total);
+
+	});
 
 }
