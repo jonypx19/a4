76b113481425d5e39d8fb241316f913e81e9eedc
George Wu <georgewu.wu@mail.utoronto.ca>
2016-07-28 18:17:10 -0400

    Added comments and posted todos for db calls

--- public/assets/scripts/getInformation.js
+++ public/assets/scripts/getInformation.js
@@ -3,14 +3,48 @@
  */
 $(document).ready(function(){
     $.ajax({
+        url: "http://localhost:3000/getFollowing",
+        type:"GET",
+        dataType:"json"
+    }).done(function(data) {
+        //Gets all the people that the current person is following.
+        console.log(data);
+        $userlist=$("<ul/>");
+        if(data.length == 0){
+            //If none exist, then place a placeholder.
+            $user=$("<li/>",{
+                text:"You aren't following anyone right now."
+            });
+            $userlist.append($user);
+        }
+        else{
+            //If there are followers, craft a link and place them into the page.
+            for (var i = 0; i < data.length; i++){
+                var user=data[i].username;
+                var email=data[i].email;
+
+                $user=$("<li/>",{
+                    html:"<a href=\"http://localhost:3000/user/"+data[i].email+"\">"+data[i].username+"</a>"
+                });
+                $userlist.append($user);
+            }
+        }
+        //If #following exists, append them. Otherwise, do nothing.
+        if($("#following").length!==0){
+            $("#following").append($userlist);
+        }
+    });
+    $.ajax({
         url: "http://localhost:3000/getComments",
         type:"GET",
         dataType:"json"
     }).done(function(data) {
+        //Gets all the comments and appends them to the main file.
         console.log(data);
         var averageRating;
         $comments = $("<ul/>");
         if(data.length == 0){
+            //If none, put a placeholder.
             averageRating = 0;
             $bullet=$("<li/>");
             $comment=$("<p/>",{
@@ -18,11 +52,13 @@ $(document).ready(function(){
             });
             $bullet.append($comment);
             $comments.append($bullet);
+            $("#comments").append($comments);
 
             //If no ratings
-            $("#rating").text("You don't have a ratings right now");
+            $("#rating").text("You don't have any ratings right now");
         }
         else{
+            //If there exists, then show them as a list.
             averageRating = 0;
             for (var i = 0; i < data.length; i++){
                 var fromText="From " + data[i].from;
@@ -38,34 +74,8 @@ $(document).ready(function(){
             }
             $("#comments").append($comments);
             averageRating = averageRating/data.length;
-            $("#rating").append(" "+averageRating);
+            $("#rating").append(" "+averageRating.toFixed(2));
         }
     });
-    $.ajax({
-        url: "http://localhost:3000/getFollowing",
-        type:"GET",
-        dataType:"json"
-    }).done(function(data) {
-        console.log(data);
-        $userlist=$("<ul/>");
-        if(data.length == 0){
-            $user=$("<li/>",{
-                text:"You aren't following anyone right now."
-            });
-            $userlist.append($user);
-        }
-        else{
-            for (var i = 0; i < data.length; i++){
-                var user=data[i].username;
-                var email=data[i].email;
 
-                $user=$("<li/>",{
-                    html:"<a href=\"http://localhost:3000/user/"+data[i].email+"\">"+data[i].username+"</a>"
-                });
-                $userlist.append($user);
-            }
-        }
-        $("#following").append($userlist);
-
-    });
 });
