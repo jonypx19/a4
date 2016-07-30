7b0887751158db03051532b894238c3cc14e2102
jonypx19 <jonathan.pelastine@mail.utoronto.ca>
2016-07-30 13:35:09 -0400

    Deleted follower email
    Not necessary for initiating a follow request

--- public/assets/scripts/fetchUsers.js
+++ public/assets/scripts/fetchUsers.js
@@ -49,11 +49,8 @@ function addUser(email){
     // Step 1: get the email of the person whom we want to follow
     var leaderEmail = getEmailFromCurrentURL();
 
-    // Step 2: get the email of the current user
-    var followerEmail = req.session.email;
-
     $.ajax({
-        url:"http://localhost:3000/addFollower/" + leaderEmail + followerEmail,
+        url:"http://localhost:3000/addFollower",
         type:"POST",
         error: function(XMLHttpRequest, textStatus, errorThrown) { 
         console.log("Status: " + textStatus); 
